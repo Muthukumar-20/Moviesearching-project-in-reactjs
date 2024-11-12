15ffe97ca953667bc2fe45d27581fd551fc150d9
Muthu kumar <mk2778555@gmail.com>
2024-11-12 21:51:52 +0530

    update

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -5,66 +5,6 @@ import { Carousel } from "flowbite-react";
 
 const Homepage = () => {
     const searchList = document.getElementById('search-list');
-
-
-
-    // function findMovies() {
-    //     let searchTerm = (movieSearchBox.value).trim();
-    //     if (searchTerm.length > 0) {
-    //         searchList.classList.remove('hide-search-list');
-    //         loadMovies(searchTerm);
-    //     } else {
-    //         searchList.classList.add('hide-search-list');
-    //     }
-    // }
-
-
-    // function displayMovieList(movies) {
-    //     searchList.innerHTML = "";
-    //     for (let idx = 0; idx < movies.length; idx++) {
-    //         let movieListItem = document.createElement('div');
-    //         movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
-    //         movieListItem.classList.add('search-list-item');
-    //         if (movies[idx].Poster != "N/A")
-    //             moviePoster = movies[idx].Poster;
-    //         else
-    //             moviePoster = "image_not_found.png";
-
-    //         movieListItem.innerHTML = `
-    //     <div class = "search-item-thumbnail">
-    //         <img src = "${moviePoster}">
-    //     </div>
-    //     <div class = "search-item-info">
-    //         <h3>${movies[idx].Title}</h3>
-    //         <p>${movies[idx].Year}</p>
-    //     </div>
-    //     `;
-    //         searchList.appendChild(movieListItem);
-    //     }
-    //     loadMovieDetails();
-    // }
-
-    // function loadMovieDetails() {
-    //     const searchListMovies = searchList.querySelectorAll('.search-list-item');
-    //     searchListMovies.forEach(movie => {
-    //         movie.addEventListener('click', async () => {
-    //             // console.log(movie.dataset.id);
-    //             searchList.classList.add('hide-search-list');
-    //             movieSearchBox.value = "";
-    //             const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=79b25d3
-
-
-    //             `);
-    //             const movieDetails = await result.json();
-    //             // console.log(movieDetails);
-    //             displayMovieDetails(movieDetails);
-    //         });
-    //     });
-    // }
-
-
-
-
     return (
         <>
             <Navbar />
